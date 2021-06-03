a97b0ccfc534b77a3ed1363b68b30a93d473ba51
chanlakSou <chanlak@example.com>
2021-06-04 00:16:47 +0700

    3rd Commit

---
+++ resources/views/authen/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.authen')
+
+@section('content')
+    <h1> Home</h1>
+
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('homepage');
 });
+// Route::get('/authen.home', function () {
+//     return view('/authen.home');
+// })->name('authen.home');
+// });
